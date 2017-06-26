d42acced818c18cfdb690aca3458bb8575c31b7d
Fergus Kelley <i@FergK.com>
2017-06-26 04:26:21 -0400

    Working on LLRBTree

--- js/LeftLeaningRedBlackTree.js
+++ js/LeftLeaningRedBlackTree.js
@@ -16,6 +16,12 @@ class LLRBNode {
 		this.right = null;
 		this.parent = null;
 	}
+
+	flipColors() {
+		this.isRed = !this.isRed;
+		this.left.isRed = !this.left.isRed;
+		this.right.isRed = !this.right.isRed;
+	}
 }
 
 class LLRB {
@@ -47,7 +53,7 @@ class LLRB {
 	search(key) {
 		var x = this.root;
 		while( x !== null ) {
-			int cmp = this.compare(key, x.key);
+			var cmp = this.compare(key, x.key);
 			if (cmp == 0) { return x.val; }
 			else if (cmp < 0) { x = x.left;	}
 			else if (cmp > 0) { x = x.right; }
@@ -56,17 +62,24 @@ class LLRB {
 	}
 
 	insert(h, key, value) {
-		if (h == null) { return new Node(key, value); }
+		if (h == null) { return new LLRBNode(key, value); }
 
-		if (h.left.isRed && h.right.isRed) { h.isRed = !h.isRed; }
+		if ( (h.left !== null) && h.left.isRed && (h.right !== null) && h.right.isRed) { h.isRed = !h.isRed; }
 
-		int cmp = this.compare(key, h.key);
+		var cmp = this.compare(key, h.key);
 		if (cmp == 0) { h.value = value; }
-		else if (cmp < 0) { h.left = insert(h.left, key, value); }
-		else { h.right = insert(h.right, key, value); }
+		else if (cmp < 0) { h.left = this.insert(h.left, key, value); }
+		else { h.right = this.insert(h.right, key, value); }
 
-		if (h.right.isRed && !h.left.isRed) { h = rotateLeft(h); }
-		if (h.left.isRed && h.left.left) { h = rotateRight(h); }
+		if (h.right.isRed && !h.left.isRed) { h = this.rotateLeft(h); }
+		if (h.left.isRed && h.left.left) { h = this.rotateRight(h); }
 
+		return h;
 	}
 }
+
+// var foo = new LLRB( 50, 50, function(a, b) { return a - b; });
+// foo.insert( foo.root, 25, 25 );
+// foo.insert( foo.root, 10, 10 );
+
+// console.log(foo);
